e31ef49469e1f0a28cb378193c60d6aa0e554b6b
bintang2629 <bimbimpratama23@gmail.com>
2026-04-07 15:44:39 +0700

    Add files via upload

---
+++ struct.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct orang{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main()
+{
+    //deklarasi obyek dari struct orang 
+    orang mhs;
+    //isi objek
+cout << "Nama Mahasiswa:";
+cin >> mhs.nama;
+cout << "Alamat Asal:";
+cin >> mhs.alamat;
+cout << "Umur Mahasiswa:";
+cin >> mhs.umur;
+cout << endl;
+//manampilkan isi objek
+cout << "Nama : " << mhs.nama << endl;
+cout << "Alamat : " << mhs.alamat << endl;
+cout << "Umur : " << mhs.umur << endl;
+
+}
+
